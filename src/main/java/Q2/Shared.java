a48c8d5cf397c6768fe0148d5221bf213ae368ba
samangishamim <samangi624@gmail.com>
2024-02-20 14:04:58 +0330

    create a 2 method for showing the sleep method in another class

--- src/main/java/Q2/Shared.java
+++ src/main/java/Q2/Shared.java
@@ -1,4 +1,17 @@
 package Q2;
 
 public class Shared {
+    synchronized void test1(Shared s2){
+        System.out.println("test 1 is begin  ");
+        Util.sleep(1000);
+
+        s2.test2();
+        System.out.println("test 1 is enddddd");
+    }
+
+    synchronized  void test2(){
+        System.out.println("test 2 is begin ");
+        Util.sleep(1000);
+        System.out.println("test 2 is enddddd");
+    }
 }
