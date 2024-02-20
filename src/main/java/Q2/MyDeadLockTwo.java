ac1cdb3b9d709d69cf44c9e31ece9c30b5cbef59
samangishamim <samangi624@gmail.com>
2024-02-20 20:52:40 +0330

    create a new class for deadlock and add all the obj

---
+++ src/main/java/Q2/MyDeadLockTwo.java
@@ -0,0 +1,43 @@
+package Q2;
+
+public class MyDeadLockTwo {
+    private static final Object resource1 = new Object();
+    private static final Object resource2 = new Object();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println("Thread 1: Locked resource 1");
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (resource2) {
+                    System.out.println("Thread 1: Locked resource 2");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println("Thread 2: Locked resource 2");
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (resource1) {
+                    System.out.println("Thread 2: Locked resource 1");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
