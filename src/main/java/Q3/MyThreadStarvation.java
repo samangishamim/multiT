bca7786814435a5f0d2942d2d2444fe3925fbf89
samangishamim <samangi624@gmail.com>
2024-02-20 20:40:49 +0330

    add new thread obj and synchronized it

--- src/main/java/Q3/MyThreadStarvation.java
+++ src/main/java/Q3/MyThreadStarvation.java
@@ -1,4 +1,26 @@
 package Q3;
 
 public class MyThreadStarvation {
+    public static void main(String[] args) {
+        Object lock = new Object();
+
+        Thread thread1 = new Thread(() -> {
+            synchronized (lock) {
+                while (true) {
+                    System.out.println("Thread 1 is running");
+                    try {
+                        Thread.sleep(1000); // Release the lock for 1 second
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+
+        // Start both threads
+        thread1.start();
+//        thread2.start();
+    }
 }
