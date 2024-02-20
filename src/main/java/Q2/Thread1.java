132fe3fe77affd88d60d2a2b5652e872c9322040
samangishamim <samangi624@gmail.com>
2024-02-20 14:08:16 +0330

    add 2 obj and create constructor

--- src/main/java/Q2/Thread1.java
+++ src/main/java/Q2/Thread1.java
@@ -1,4 +1,17 @@
 package Q2;
 
-public class Thread1 {
+public class Thread1  extends  Thread{
+    private  final Shared s1;
+    private  final  Shared s2;
+
+
+    public Thread1(Shared s1, Shared s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    @Override
+    public void run() {
+        s1.test1(s2);
+    }
 }
