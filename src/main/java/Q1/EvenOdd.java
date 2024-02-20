69405a079b33b1f8b73a5f31cf2cbfa4645a0292
samangishamim <samangi624@gmail.com>
2024-02-20 22:54:23 +0330

    fix the bug

--- src/main/java/Q1/EvenOdd.java
+++ src/main/java/Q1/EvenOdd.java
@@ -1,38 +1,40 @@
 package Q1;
 
+import java.util.ArrayList;
+
 public class EvenOdd {
     String State;
+    ArrayList<Integer> list = new ArrayList<>();
 
-    synchronized public void Even(boolean running) {
+    synchronized public void Even(boolean running, int i) {
         if (!running) {//stop
 
             State = "even";
             notify();//harchi wait stop
             return;
         }
-
-        System.out.println("even ");
-        State = "even ";
+        list.add(i);
+        State = "even";
         notify();
         try {
-            while (!State.equals("even"))
+            while (!State.equals("odd"))
                 wait();
         } catch (InterruptedException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    synchronized public void Odd(boolean running) {
+    synchronized public void Odd(boolean running,int i) {
         if (!running) {
             State = "odd";
             notify();
             return;
         }
-        System.out.println("odd ");
-        State = "odd ";
+        list.add(i);
+        State = "odd";
         notify();
         try {
-            while (!State.equals("odd"))
+            while (!State.equals("even"))
                 wait();
         } catch (InterruptedException e) {
             System.out.println(e.getMessage());
