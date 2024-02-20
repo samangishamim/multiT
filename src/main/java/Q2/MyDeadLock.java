6443066b5fdca5032bec93ba982cf1afb9340c82
samangishamim <samangi624@gmail.com>
2024-02-20 16:18:35 +0330

    fix bug

--- src/main/java/Q2/MyDeadLock.java
+++ src/main/java/Q2/MyDeadLock.java
@@ -1,4 +1,17 @@
 package Q2;
 
 public class MyDeadLock {
+    public static void main(String[] args) {
+        Shared s1=new Shared();
+        Shared s2=new Shared();
+
+        Thread1 t1=new Thread1(s1,s2);
+        t1.start();
+
+        Thread2 t2=new Thread2(s1,s2);
+        t2.start();
+
+
+        Util.sleep(100);
+    }
 }
--- src/main/java/Q2/Shared.java
+++ src/main/java/Q2/Shared.java
@@ -3,7 +3,7 @@ package Q2;
 public class Shared {
     synchronized void test1(Shared s2){
         System.out.println("test 1 is begin  ");
-        Util.sleep(1000);
+        Util.sleep(100);
 
         s2.test2();
         System.out.println("test 1 is enddddd");
@@ -11,7 +11,7 @@ public class Shared {
 
     synchronized  void test2(){
         System.out.println("test 2 is begin ");
-        Util.sleep(1000);
+        Util.sleep(100);
         System.out.println("test 2 is enddddd");
     }
 }
