8e67a2c6904bb1f7658b1949fa914d999a42e428
samangishamim <samangi624@gmail.com>
2024-02-20 13:46:24 +0330

    fix the run method

--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -1,2 +1,25 @@
-public class MyThread {
+import java.util.List;
+
+public class MyThread implements Runnable {
+    private final List<Integer> sharedList;
+    private final boolean isEven;
+    private final int maxNumber;
+
+    public MyThread(List<Integer> sharedList, boolean isEven, int maxNumber) {
+        this.sharedList = sharedList;
+        this.isEven = isEven;
+        this.maxNumber = maxNumber;
+    }
+
+    @Override
+    public void run() {
+        synchronized (sharedList) {
+            for (int i = isEven ? 0 : 1; i <= maxNumber; i += 2) {
+                if ((isEven && i % 2 == 0) || (!isEven && i % 2 == 1)) {
+                    sharedList.add(i);
+                }
+            }
+            sharedList.notify();
+        }
+    }
 }
