4fdf12090e56acf4a1b4ec71c50524b3140289cf
samangishamim <samangi624@gmail.com>
2024-02-20 22:53:33 +0330

    fix the bug

--- src/main/java/Q1/MyThread.java
+++ src/main/java/Q1/MyThread.java
@@ -5,20 +5,21 @@ public class MyThread implements Runnable {
     Thread thread;
     int input;
 
-    public MyThread(String name, EvenOdd evenOdd) {
+    public MyThread(String name, EvenOdd evenOdd, int input) {
         thread = new Thread(this, name);
         this.evenOdd = evenOdd;
+        this.input = input;
     }
 
     @Override
     public void run() {
         if (thread.getName().compareTo("Even") == 0) {
-            for (int i = 0; i <=input; i += 2) evenOdd.Even(true);
-            evenOdd.Even(false);
+            for (int i = 0 ; i <= input ; i += 2) evenOdd.Even(true,i);
+            evenOdd.Even(false,-1);
         } else {
-            for (int i = 0; i < input; i += 2) evenOdd.Odd(true);
+            for (int i = 1 ; i <= input ; i += 2) evenOdd.Odd(true,i);
             {
-                evenOdd.Odd(false);
+                evenOdd.Odd(false,-1);
             }
         }
     }
