9adbcba946faafffb229b393efeba056977ec7ce
samangishamim <samangi624@gmail.com>
2024-02-20 12:50:58 +0330

    create 2 methods for the showing numbers

--- src/main/java/Multi.java
+++ src/main/java/Multi.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Multi {
+    String state;
+
+  public  void even(){
+      System.out.println("even: ");
+      state="evenNumber";
+    }
+
+    public void odd(){
+        System.out.println("odd: ");
+        state="oddNumber";
+    }
 }
