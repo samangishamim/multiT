f9aeba98652e3246fe630571354c15ff5ea802e0
samangishamim <samangi624@gmail.com>
2024-02-20 14:01:23 +0330

    create a sleep method

--- src/main/java/Q2/Util.java
+++ src/main/java/Q2/Util.java
@@ -1,4 +1,11 @@
 package Q2;
 
 public class Util {
+    static void sleep(long millis){
+        try {
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+        e.printStackTrace();
+        }
+    }
 }
