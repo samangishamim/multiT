869c07e9f2f7694189f76630347e04d36e1a49fa
samangishamim <samangi624@gmail.com>
2024-02-20 15:53:18 +0330

    add new threads

--- src/main/java/Q1/Main.java
+++ src/main/java/Q1/Main.java
@@ -1,4 +1,12 @@
 package Q1;
 
 public class Main {
+    public static void main(String[] args) {
+        EvenOdd evenOdd=new EvenOdd();
+        MyThread t1=new MyThread("Even",evenOdd);
+        t1.thread.start();
+
+        MyThread t2=new MyThread("odd",evenOdd);
+        t2.thread.start();
+    }
 }
