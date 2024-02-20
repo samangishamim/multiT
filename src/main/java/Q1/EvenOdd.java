b1a320ce702e24486a7ccfed2d11cab8a56e05a0
samangishamim <samangi624@gmail.com>
2024-02-20 15:47:37 +0330

    add even and odd method

--- src/main/java/Q1/EvenOdd.java
+++ src/main/java/Q1/EvenOdd.java
@@ -1,4 +1,14 @@
 package Q1;
 
 public class EvenOdd {
+    String State;
+    public  void Even (){
+        System.out.println("even ");
+     State="even " ;
+    }
+
+    public  void Odd (){
+        System.out.println("odd ");
+        State="odd " ;
+    }
 }
